9850c6183d8229c091773f070b507752e645940c
adriananghelclaudiu <anghel.adrian.claudiu.89@gmail.com>
2021-02-17 17:43:54 +0200

    Testare modificare

--- src/ro/java/ctrln/Main.java
+++ src/ro/java/ctrln/Main.java
@@ -2,6 +2,6 @@ package ro.java.ctrln;
 
 public class Main {
     public static void main(String[] args) {
-	    System.out.println("Invat limbajul Java si despre masina virtuala Java!");
+	    System.out.println("Invat limbajul Java si despre masina virtuala Java! ");
     }
 }
